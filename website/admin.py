e426a63518fbb75e98b7f0078d0cc21a518210c8
YoungKhalif <128808381+YoungKhalif@users.noreply.github.com>
2025-04-09 16:30:52 +0000

    Delete website directory

--- website/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost, ContactMessage
-
-# Register your models here.
-admin.site.register(BlogPost)
-admin.site.register(ContactMessage)
--- website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, blog_list, contact_view
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('blog/', blog_list, name='blog_list'),
-    path('contact/', contact_view, name='contact'),
-]
